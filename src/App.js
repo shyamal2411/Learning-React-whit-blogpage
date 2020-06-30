6246e7f36b5d583eb66a9212f0854b1e73a9faf5
vacoman <juan-sebastian1997@outlook.com>
2020-06-30 09:21:17 -0500

    Esta es la primera practica para aprender JavaScripts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className="App-header">
+        <div className="classContent">
+          <h1>Formulario</h1>
+          <p>hola mundo</p>
+          <form action="firebase" name="formulario" method="POST">
+            <input type="checkbox" name="dsdsdsddsd" id="aceptar"/>l
+            <button type="submit" onClick= "validar" id='btn' name='btn' value="hola"/>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Slide.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Slider() {
+    let clave='Sebastian Fajardo';
+    return(
+        <h1>hola mundo {clave}</h1>
+
+    );
+}
+
+export default Slider;
