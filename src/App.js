a66c08c735fd509c8082eef93d0b68c74633071c
vacoman <juan-sebastian1997@outlook.com>
2020-06-30 16:15:12 -0500

    re estructuracion usando git

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from "react";
 import "./App.css";
+import Slider from "./Slide";
 
 
 class App extends React.Component{
   render(){
     return(
-      <div className="App-header">
-        <div className="classContent">
-          <h1>Formulario</h1>
-          <p>hola mundo</p>
-          <form action="firebase" name="formulario" method="POST">
-            <input type="checkbox" name="dsdsdsddsd" id="aceptar"/>l
-            <button type="submit" onClick= "validar" id='btn' name='btn' value="hola"/>
-          </form>
-        </div>
-      </div>
-    );
+      Slider()
+      );
   }
 }
 export default App;
