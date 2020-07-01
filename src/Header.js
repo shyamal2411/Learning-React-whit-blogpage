262882a43f999af79c797fd7e4cb78d3bf7fd6c3
vacoman <juan-sebastian1997@outlook.com>
2020-07-01 14:17:40 -0500

    intento de merge

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
+//import logo from '../public/Recurso-1.png';
 
 function Header() {
     return (
         <div className="app-header">
             <div className="logo">
-            	<a href="#" alt="Profe-Ana"><img src="../public\Recurso-1.png"></img></a>
+            	<a href="#" alt="Profe-Ana"><img src=""></img></a>
             </div>
             <div>
                 <div>
