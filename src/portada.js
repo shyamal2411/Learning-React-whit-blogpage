85f835e235b558fdc199173726517e73c13beecb
vacoman <juan-sebastian1997@outlook.com>
2020-07-18 22:44:22 -0500

    agregar  favicon + portada

---
+++ src/portada.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './stilosPortada.css';
+
+function PortadaImg (){
+    return(
+        <div className="totalPortada">
+            <div className="imgPortada">
+                <img className="imgPortadala" src="https://i.pinimg.com/originals/fe/d5/23/fed5236c27c4d159a7cadca910d93da2.jpg" alt="Fondo"/>
+            </div>
+
+        </div>
+    );
+}
+
+export default PortadaImg;
